6f281ed1aab9cc429cf36b42a97805d2824ae419
bigtit@outlook.com <bigtit@outlook.com>
2018-03-24 14:47:46 -0400

    add usage

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+if [ "$#" -ne 2 ]; then
+  echo "usage: $0 [hdd image]"
+  exit -1
+fi
+
 /usr/bin/qemu-system-x86_64 -cpu host -m 4096 -enable-kvm -drive file=$1,format=qcow2,if=virtio,cache=writeback -netdev user,id=net0,hostfwd=tcp::2222-:22 -device e1000,netdev=net0
--- runtap.sh
+++ runtap.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+if [ "$#" -ne 2 ]; then
+  echo "usage: $0 [hdd image]"
+  exit -1
+fi
+
 /usr/bin/qemu-system-x86_64 -cpu kvm64 -m 4096 -enable-kvm -drive file=$1,format=qcow2,if=virtio,cache=writeback -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device e1000,netdev=net0
