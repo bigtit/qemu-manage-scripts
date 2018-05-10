9c015950d6330138d4befc30eea3e82a6ce7eb28
kfang2 <kfang2@binghamton.edu>
2018-05-10 19:11:05 -0400

    redir port

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ "$#" -ne 1 ]; then
-  echo "usage: $0 [hdd image]"
+if [ "$#" -ne 2 ]; then
+  echo "usage: $0 [hdd image] [nat_redir port]"
   exit -1
 fi
 
-/usr/bin/qemu-system-x86_64 -cpu host -m 4096 -enable-kvm -drive file=$1,format=qcow2,if=virtio,cache=writeback -netdev user,id=net0,hostfwd=tcp::2222-:22 -device e1000,netdev=net0
+/usr/bin/qemu-system-x86_64 -cpu host -m 4096 -enable-kvm -drive file=$1,format=qcow2,if=virtio,cache=writeback -netdev user,id=net0,hostfwd=tcp::$2-:22 -device e1000,netdev=net0
